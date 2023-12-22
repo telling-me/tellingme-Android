9db3fac7ab921dd9ed0395ff066fd0267f08dab7
Ojongseok <whdtjr6889@naver.com>
2023-12-22 12:29:53 +0900

    [Rename] MainActivity 경로 변경

--- app/src/main/java/com/example/tellingme/presentation/ui/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/tellingme/presentation/ui/feature/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tellingme
+package com.example.tellingme.presentation.ui.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.tellingme.ui.theme.TellingMeTheme
+import com.example.tellingme.presentation.ui.theme.TellingMeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
