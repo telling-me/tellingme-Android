862eb683fad6752ca42529aee6208cf78a61d8b8
Ojongseok <whdtjr6889@naver.com>
2023-12-22 09:39:42 +0900

    [Build] Retrofit, OkHttp 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,13 @@ android {
 }
 
 dependencies {
+
+    // Retrofit, OkHttp
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.8.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.8.0")
+
     // Hilt
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
