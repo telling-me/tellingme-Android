c6f51ace10a6ebed7e30d16a71bdaedc2921316e
Ojongseok <whdtjr6889@naver.com>
2023-12-31 20:02:04 +0900

    feature: Chip 컴포넌트 패키지 및 .kt 생성

---
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/common/chip/ChoiceChip.kt
@@ -0,0 +1,11 @@
+package com.tellingus.tellingme.presentation.ui.common.chip
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChoiceChip(
+    modifier: Modifier = Modifier
+) {
+
+}
