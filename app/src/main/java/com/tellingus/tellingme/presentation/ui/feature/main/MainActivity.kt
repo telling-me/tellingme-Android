b1daa2d48cbdbb233e4de4b35915afd02ea071ff
Ojongseok <whdtjr6889@naver.com>
2024-01-05 14:42:16 +0900

    feature: FloatingButton 컴포넌트 추가

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/main/MainActivity.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/main/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tellingus.tellingme.presentation.ui.common.button.BUTTON_SIZE
+import com.tellingus.tellingme.presentation.ui.common.button.FloatingButton
 import com.tellingus.tellingme.presentation.ui.common.button.PrimaryButton
 import com.tellingus.tellingme.presentation.ui.common.button.PrimaryLightButton
 import com.tellingus.tellingme.presentation.ui.common.button.SecondaryButton
@@ -67,6 +68,9 @@ fun Greeting() {
             text = "버튼버튼",
             onClick = { }
         )
+        FloatingButton {
+
+        }
     }
 }
 
