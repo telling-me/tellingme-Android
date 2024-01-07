cb6448a6f8ee627df6e0ca20a5a651c9502e3b68
장현봉 <wkdgusqhd080@naver.com>
2024-01-07 20:32:43 +0900

    feature: 기록하기 화면 추가

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/main/home/HomeScreen.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/main/home/HomeScreen.kt
@@ -1,11 +1,17 @@
 package com.tellingus.tellingme.presentation.ui.feature.main.home
 
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(
+    navigateToOtherSpace: (name: String) -> Unit
 ) {
     Text(text = "홈")
+
+    Button(onClick = { navigateToOtherSpace("name") }) {
+        Text(text = "더보기")
+    }
 }
