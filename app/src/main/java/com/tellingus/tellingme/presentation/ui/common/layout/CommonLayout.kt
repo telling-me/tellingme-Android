efc6052d213382ca4280bfebe8941d9b6ba335dd
장현봉 <wkdgusqhd080@naver.com>
2024-01-09 20:10:39 +0900

    feature: MainLayout header영역 수정

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/common/layout/CommonLayout.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/common/layout/CommonLayout.kt
@@ -1,14 +1,25 @@
 package com.tellingus.tellingme.presentation.ui.common.layout
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import com.tellingus.tellingme.presentation.ui.common.header.Header
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainLayout(header: @Composable () -> Unit = { Header() }, content: @Composable () -> Unit) {
-    Scaffold(topBar = { header() }, content = { content() })
+fun MainLayout(header: @Composable (() -> Unit)? = null, content: @Composable () -> Unit) {
+    Scaffold(
+        topBar = { header?.let { header() } },
+        content = {
+            Box(modifier = Modifier.padding(top = if (header != null) 48.dp else 0.dp)) {
+                content()
+            }
+
+        }
+    )
 }
--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/home/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.tellingus.tellingme.presentation.ui.common.appbar.TellingMeAppBar
 import com.tellingus.tellingme.presentation.ui.common.layout.MainLayout
 import com.tellingus.tellingme.presentation.ui.common.widget.LevelSection
 import com.tellingus.tellingme.presentation.ui.common.widget.ProfileWidget
@@ -23,27 +24,30 @@ import com.tellingus.tellingme.presentation.ui.theme.TellingmeTheme
 fun HomeScreen(
     navigateToOtherSpace: (name: String) -> Unit
 ) {
-    MainLayout(content = {
-        Column(
-            modifier = Modifier
-                .padding(top = 56.dp)
-                .fillMaxSize()
-                .background(Background100)
-        ) {
-            Box(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
-                ProfileWidget(nickname = "듀이듀이", description = "연속 1일째 기록")
+    MainLayout(
+        header = { TellingMeAppBar(title = "헤더영역") },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Background100)
+            ) {
+                Box(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
+                    ProfileWidget(nickname = "듀이듀이", description = "연속 1일째 기록")
+                }
+                Box(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp)) {
+                    LevelSection(level = 1, percent = 50)
+                }
+                Column(modifier = Modifier.padding(top = 32.dp, start = 20.dp, end = 20.dp)) {
+                    Text(text = "오늘의 질문", style = TellingmeTheme.typography.body1Bold)
+                    Text(
+                        modifier = Modifier.padding(top = 5.dp),
+                        text = "12명이 대답했어요!",
+                        style = TellingmeTheme.typography.caption1Regular
+                    )
+                }
             }
-            Box(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp)) {
-                LevelSection(level = 1, percent = 50)
-            }
-            Column(modifier = Modifier.padding(top = 32.dp, start = 20.dp, end = 20.dp)) {
-                Text(text = "오늘의 질문", style = TellingmeTheme.typography.body1Bold)
-                Text(
-                    modifier = Modifier.padding(top = 5.dp),
-                    text = "12명이 대답했어요!",
-                    style = TellingmeTheme.typography.caption1Regular
-                )
-            }
-        }
-    })
+        })
 }
+
+
