81d000e79465495e81b31e9f06c88dbc3642ed2e
장현봉 <wkdgusqhd080@naver.com>
2024-01-13 19:28:33 +0900

    feature: MainLayout background paramter 추가

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/common/layout/MainLayout.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/common/layout/MainLayout.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.tellingus.tellingme.presentation.ui.theme.Background100
 
@@ -17,16 +18,17 @@ import com.tellingus.tellingme.presentation.ui.theme.Background100
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainLayout(
-    contentModifier: Modifier = Modifier.background(Background100),
     header: @Composable (() -> Unit)? = null,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
+    background: Color = Background100,
 ) {
     Scaffold(
         topBar = { header?.let { header() } },
         content = {
             Box(
-                modifier = contentModifier
+                modifier = Modifier
                     .padding(top = if (header != null) 48.dp else 0.dp)
+                    .background(background)
                     .verticalScroll(
                         rememberScrollState()
                     )
