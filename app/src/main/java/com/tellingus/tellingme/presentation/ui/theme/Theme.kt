519335f080e732dbb116388e68dec55bd75ab1f4
Ojongseok <whdtjr6889@naver.com>
2024-01-19 17:25:46 +0900

    chore: 시스템 다크모드 대응되지 않도록 Theme 수정

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/theme/Theme.kt
@@ -35,14 +35,15 @@ fun TellingmeTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
+//    val colorScheme = when {
+//      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//        val context = LocalContext.current
+//        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//      }
+//      darkTheme -> DarkColorScheme
+//      else -> LightColorScheme
+//    }
     val view = LocalView.current
     if (!view.isInEditMode) {
       SideEffect {
