f2f396bdcfd45c8df147889869fa58608f6b6c11
Ojongseok <whdtjr6889@naver.com>
2024-01-22 15:06:55 +0900

    chore: 글로벌 Application 설정 및 카카오 NativeKey 입력

--- app/src/main/java/com/tellingus/tellingme/util/TellingmeApplication.kt
+++ app/src/main/java/com/tellingus/tellingme/util/TellingmeApplication.kt
@@ -1,9 +1,15 @@
 package com.tellingus.tellingme.util
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class TellingmeApplication: Application() {
+class TellingMeApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
 
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, "c515a0c1559c52e253c9985aee676a05")
+    }
 }
