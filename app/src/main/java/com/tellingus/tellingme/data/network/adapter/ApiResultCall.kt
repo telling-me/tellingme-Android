80006f8f1a125ebd6048d1b0792209bfd601c8a6
Ojongseok <whdtjr6889@naver.com>
2024-01-30 19:53:49 +0900

    feature: ApiResult 정의 및 Retroift에 적용

---
+++ app/src/main/java/com/tellingus/tellingme/data/network/adapter/ApiResultCall.kt
@@ -0,0 +1,74 @@
+package com.tellingus.tellingme.data.network.adapter
+
+import com.google.gson.Gson
+import okhttp3.Request
+import okio.Timeout
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ApiResultCall<T>(private val delegate: Call<T>) : Call<ApiResult<T>> {
+    private val gson = Gson()
+    override fun enqueue(callback: Callback<ApiResult<T>>) {
+        delegate.enqueue(
+            object : Callback<T> {
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    if (response.isSuccessful) {
+                        callback.onResponse(
+                            this@ApiResultCall,
+                            Response.success(
+                                ApiResult.Success(response.body()!!)
+                            )
+                        )
+                    } else {
+                        callback.onResponse(
+                            this@ApiResultCall,
+                            Response.success(
+                                ApiResult.Failure(response.message())
+                            )
+                        )
+                    }
+                }
+
+                // 서버에서 받는 에러는 모두 Response.success로 감싸져 옴
+                // 따라서, onFailure가 호출되는 경우는 인터넷 연결 오류밖에 없음
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    callback.onResponse(
+                        this@ApiResultCall,
+                        Response.success(
+                            ApiResult.NetworkError(t)
+                        )
+                    )
+                }
+            }
+        )
+    }
+
+    override fun isExecuted(): Boolean {
+        return delegate.isExecuted
+    }
+
+    override fun execute(): Response<ApiResult<T>> {
+        throw UnsupportedOperationException("ResponseCall does not support execute.")
+    }
+
+    override fun cancel() {
+        delegate.cancel()
+    }
+
+    override fun isCanceled(): Boolean {
+        return delegate.isCanceled
+    }
+
+    override fun clone(): Call<ApiResult<T>> {
+        return ApiResultCall(delegate.clone())
+    }
+
+    override fun request(): Request {
+        return delegate.request()
+    }
+
+    override fun timeout(): Timeout {
+        return delegate.timeout()
+    }
+}
