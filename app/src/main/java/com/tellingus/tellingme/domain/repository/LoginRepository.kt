58acb76f3884537f22cfef0a70ff47e734eb4dcd
Ojongseok <whdtjr6889@naver.com>
2024-02-01 22:06:24 +0900

    rename: User -> Login으로 Repository, ViewModel 네이밍 변경

--- app/src/main/java/com/tellingus/tellingme/domain/repository/LoginRepository.kt
+++ app/src/main/java/com/tellingus/tellingme/domain/repository/LoginRepository.kt
@@ -3,9 +3,8 @@ package com.tellingus.tellingme.domain.repository
 import com.tellingus.tellingme.data.model.login.OauthRequestDto
 import com.tellingus.tellingme.data.model.login.TokenDto
 import com.tellingus.tellingme.data.network.adapter.ApiResult
-import kotlinx.coroutines.flow.Flow
 
-interface UserRepository {
+interface LoginRepository {
     suspend fun loginFromKakao(
         oauthToken: String,
         loginType: String,
