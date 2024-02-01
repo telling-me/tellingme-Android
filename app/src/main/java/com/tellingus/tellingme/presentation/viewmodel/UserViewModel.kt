49a508d120f3477b63a67490d75d48f6017c1068
Ojongseok <whdtjr6889@naver.com>
2024-02-01 19:05:08 +0900

    feature: DataStore모듈 ViewModel에서 읽기/쓰기 작동 확인

--- app/src/main/java/com/tellingus/tellingme/presentation/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/viewmodel/UserViewModel.kt
@@ -7,14 +7,21 @@ import com.tellingus.tellingme.data.model.login.OauthRequestDto
 import com.tellingus.tellingme.data.network.adapter.onFailure
 import com.tellingus.tellingme.data.network.adapter.onNetworkError
 import com.tellingus.tellingme.data.network.adapter.onSuccess
+import com.tellingus.tellingme.data.repositoryimpl.DataStoreRepositoryImpl
 import com.tellingus.tellingme.domain.usecase.LoginUseCase
 import com.tellingus.tellingme.util.TAG
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(
+class UserViewModel @Inject constructor(
+    private val dataStoreRepositoryImpl: DataStoreRepositoryImpl,
     private val loginUseCase: LoginUseCase
 ): ViewModel() {
 
@@ -43,8 +50,18 @@ class LoginViewModel @Inject constructor(
                 .onFailure { message, code ->
                     when(code) {
                         404 -> {
-                            Log.d(TAG, message)
-                            Log.d(TAG, message.split("${'"'}")[3])
+                            val socialId = message.split("${'"'}")[3]
+                            Log.d(TAG, "$message // socialId : $socialId")
+
+                            // 소셜로그인 로컬에 저장하고 읽기
+                            CoroutineScope(Dispatchers.IO).launch {
+                                dataStoreRepositoryImpl.setUserSocialId(socialId)
+                                withContext(Dispatchers.IO) {
+                                    dataStoreRepositoryImpl.getUserSocialId().collectLatest {
+                                        Log.d(TAG+"1", it)
+                                    }
+                                }
+                            }
                         }
                         1000 -> {
                             Log.d(TAG, code.toString())
@@ -60,7 +77,16 @@ class LoginViewModel @Inject constructor(
 
         }
     }
+
+    private fun saveUserSocialId(socialId: String) {
+        viewModelScope.launch {
+            dataStoreRepositoryImpl.setUserSocialId(socialId)
+        }
+    }
+
 }
+
+
 
 enum class LoginType(name: String) {
     KAKAO("kakao")
