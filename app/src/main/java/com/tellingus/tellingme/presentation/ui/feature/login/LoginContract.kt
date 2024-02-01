a094dfa816246c36dc401843f3e3eaca4fac4e54
Ojongseok <whdtjr6889@naver.com>
2024-02-01 22:33:31 +0900

    feature: 로그인과 관련된 State와 Effect를 추적하는 LoginContract 정의

---
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/login/LoginContract.kt
@@ -0,0 +1,18 @@
+package com.tellingus.tellingme.presentation.ui.feature.login
+
+import com.tellingus.tellingme.data.model.login.JoinRequestDto
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+interface LoginContract {
+    val joinRequestState: StateFlow<JoinRequestDto>
+    val loginUiEffect: SharedFlow<LoginUiEffect>
+}
+
+sealed class LoginUiEffect {
+    data class MoveToOauthJoin(
+        val socialId: String
+    ): LoginUiEffect()
+
+    object MoveToHome : LoginUiEffect()
+}
