bf803faa73211a198b04f5c715c7129a5baa27f2
Ojongseok <whdtjr6889@naver.com>
2024-02-01 22:31:36 +0900

    rename: 로그인 타입 구분하는 enum class 경로 변경

---
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/login/Enum.kt
@@ -0,0 +1,10 @@
+package com.tellingus.tellingme.presentation.ui.feature.login
+
+enum class LoginType(name: String) {
+    KAKAO("kakao")
+}
+
+enum class IsAuto(name: String) {
+    MANUAL("manual"),
+    AUTO("auto")
+}
