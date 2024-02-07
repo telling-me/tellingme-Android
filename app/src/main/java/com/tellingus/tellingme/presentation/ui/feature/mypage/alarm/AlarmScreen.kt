3b1eeaf8543c93934289fd659893083132567f28
장현봉 <wkdgusqhd080@naver.com>
2024-02-07 18:25:12 +0900

    feature: 알림화면 카드아이템 swipe && 삭제 버튼 추가

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/mypage/alarm/AlarmScreen.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/mypage/alarm/AlarmScreen.kt
@@ -1,43 +1,77 @@
 package com.tellingus.tellingme.presentation.ui.feature.mypage.alarm
 
 import android.annotation.SuppressLint
+import android.content.Context
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.SwipeToDismiss
+import androidx.compose.material.rememberDismissState
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
+import androidx.compose.material3.Button
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.google.firebase.annotations.concurrent.Background
 import com.tellingus.tellingme.R
 import com.tellingus.tellingme.presentation.ui.common.component.appbar.BasicAppBar
 import com.tellingus.tellingme.presentation.ui.common.component.button.TellingmeIconButton
 import com.tellingus.tellingme.presentation.ui.common.component.chip.ActionChip
 import com.tellingus.tellingme.presentation.ui.common.component.layout.MainLayout
 import com.tellingus.tellingme.presentation.ui.common.model.ButtonSize
+import com.tellingus.tellingme.presentation.ui.theme.Background100
 import com.tellingus.tellingme.presentation.ui.theme.Gray500
 import com.tellingus.tellingme.presentation.ui.theme.Gray600
 import com.tellingus.tellingme.presentation.ui.theme.Primary100
+import com.tellingus.tellingme.presentation.ui.theme.Red600
 import com.tellingus.tellingme.presentation.ui.theme.TellingmeTheme
 import com.tellingus.tellingme.provider.LocalNavController
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
 fun AlarmScreen() {
     MainLayout(
         header = { AlarmScreenHeader() },
         content = { AlarmScreenContent() },
-        isScrollable = false
+        isScrollable = false,
+        background = Color.White
     )
 }
 
@@ -58,6 +92,7 @@ fun AlarmScreenHeader() {
     )
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun AlarmScreenContent() {
     Column(
@@ -88,6 +123,7 @@ fun AlarmScreenContent() {
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun AlarmCard(
@@ -97,46 +133,110 @@ fun AlarmCard(
     date: String,
     isRead: Boolean = false
 ) {
+    val context = LocalContext.current
     val interactionSource = remember { MutableInteractionSource() }
     val isPressed by interactionSource.collectIsPressedAsState()
 
-    Column(
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val scope = rememberCoroutineScope()
+
+    val squareSize = 80.dp
+    val sizePx = with(LocalDensity.current) { squareSize.toPx() }
+
+    val anchors = mapOf(0f to 0, -sizePx to 1)
+
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 12.dp, bottom = 12.dp)
-            .alpha(if (isRead === true || isPressed === true) 0.5f else 1f)
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null,
-                onClick = {}
+            .fillMaxHeight()
+            .swipeable(
+                state = swipeableState,
+                orientation = Orientation.Horizontal,
+                anchors = anchors,
+//                anchors = mapOf(
+//                    0f to 0,
+//                    -dipToPx(context, 100f) to 1,
+//                    dipToPx(context, 100f) to 2
+//                ),
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                velocityThreshold = 1000.dp
             )
     ) {
-        Text(
-            text = getAlarmCardTypeText(alarmType),
-            color = Primary100,
-            style = TellingmeTheme.typography.caption1Bold,
-        )
-        Text(
-            text = title,
-            color = Gray600,
-            style = TellingmeTheme.typography.body2Bold,
-            modifier = Modifier.padding(top = 4.dp)
-        )
-        if (content !== "") {
-            Text(
-                text = content,
-                color = Gray600,
-                style = TellingmeTheme.typography.body2Regular,
-                modifier = Modifier.padding(top = 4.dp)
-            )
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .fillMaxHeight()
+                .background(Red600)
+
+        ) {
+            TextButton(
+                modifier = Modifier
+                    .width(80.dp)
+                    .fillMaxHeight(),
+
+                onClick = {
+                    scope.launch {
+                        swipeableState.animateTo(0, tween(600, 0))
+                    }
+                }) {
+                Text(text = "삭제", color = Color.White)
+            }
         }
-        Text(
-            text = date,
-            color = Gray600,
-            style = TellingmeTheme.typography.caption1Regular,
-            modifier = Modifier.padding(top = 4.dp)
-        )
+
+
+        Box(
+            modifier = Modifier
+                .offset {
+                    // swipe했을 때 위치가 움직이도록 offset 설정
+                    IntOffset(swipeableState.offset.value.roundToInt(), 0)
+                }
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .background(Color.White)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp, bottom = 12.dp)
+                    .alpha(if (isRead === true || isPressed === true) 0.5f else 1f)
+                    .clickable(
+                        interactionSource = interactionSource,
+                        indication = null,
+                        onClick = {}
+                    )
+            ) {
+                Text(
+                    text = getAlarmCardTypeText(alarmType),
+                    color = Primary100,
+                    style = TellingmeTheme.typography.caption1Bold,
+                )
+                Text(
+                    text = title,
+                    color = Gray600,
+                    style = TellingmeTheme.typography.body2Bold,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+                if (content !== "") {
+                    Text(
+                        text = content,
+                        color = Gray600,
+                        style = TellingmeTheme.typography.body2Regular,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
+                Text(
+                    text = date,
+                    color = Gray600,
+                    style = TellingmeTheme.typography.caption1Regular,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+
+
     }
+
 }
 
 fun getAlarmCardTypeText(alarmType: String): String {
@@ -149,12 +249,17 @@ fun getAlarmCardTypeText(alarmType: String): String {
 }
 
 data class AlarmItem(
+    val id: Int,
     val alarmType: String,
     val title: String,
     val content: String = "",
     val date: String,
     val isRead: Boolean = false,
 )
+
+private fun dipToPx(context: Context, dipValue: Float): Float {
+    return dipValue * context.resources.displayMetrics.density
+}
 
 @Preview
 @Composable
@@ -163,53 +268,67 @@ fun AlarmScreenPreview() {
 }
 
 val dummyList = listOf(
-    AlarmItem("alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.20", isRead = true),
+    AlarmItem(id = 1, "alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.20", isRead = true),
     AlarmItem(
+        id = 2,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
         "2023.09.15"
     ),
-    AlarmItem("alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.13", isRead = true),
+    AlarmItem(id = 3, "alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.13", isRead = true),
     AlarmItem(
+        id = 4,
         alarmType = "notice",
         title = "서비스 오류 공지",
         content = "현재 텔링미에 에러가 발생하고 있어요. 신속하게 처리할게요! 불편을 끼쳐뜨려 죄송합니다.",
         date = "2023.09.15"
     ),
     AlarmItem(
+        id = 5,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
         "2023.08.24"
     ),
     AlarmItem(
+        id = 6,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
         "2023.08.15"
     ),
-    AlarmItem("alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.20"),
     AlarmItem(
+        id = 7,
+        "alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.20"
+    ),
+    AlarmItem(
+        id = 8,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
         "2023.09.15"
     ),
-    AlarmItem("alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.13"),
     AlarmItem(
+        id = 9,
+        "alarm", title = "지난 달 서재가 완성되었어요!", date = "2023.09.13"
+    ),
+    AlarmItem(
+        id = 10,
         alarmType = "notice",
         title = "서비스 오류 공지",
         content = "현재 텔링미에 에러가 발생하고 있어요. 신속하게 처리할게요! 불편을 끼쳐뜨려 죄송합니다.",
         date = "2023.09.15"
     ),
     AlarmItem(
+        id = 11,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
         "2023.08.24"
     ),
     AlarmItem(
+        id = 12,
         "event",
         "텔링미북 모으는 분들 주목!",
         "더 나은 텔링미 북 제작을 위한 여러분들의 의견이 궁금합니다! 설문 조사에 참여해주세요.",
