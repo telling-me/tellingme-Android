e9d6cd6a5cec036d2e7ae24b84b79f69dc3f33da
Ojongseok <whdtjr6889@naver.com>
2024-02-09 13:46:33 +0900

    chore: 로그인 관련 enum class LoginViewModel로 이동

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/login/model/IsAuto.kt
+++
@@ -1,6 +0,0 @@
-package com.tellingus.tellingme.presentation.ui.feature.login.model
-
-enum class IsAuto(name: String) {
-    MANUAL("manual"),
-    AUTO("auto")
-}
--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/login/model/LoginType.kt
+++
@@ -1,5 +0,0 @@
-package com.tellingus.tellingme.presentation.ui.feature.login.model
-
-enum class LoginType(name: String) {
-    KAKAO("kakao")
-}
