559394b22192456478ef8fa20a3517d6d1ce17aa
Ojongseok <whdtjr6889@naver.com>
2024-02-10 08:11:21 +0900

    chore: 로그인 로직 홈화면에서 로그인 화면으로 이동

---
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/home/HomeViewModel.kt
@@ -0,0 +1,10 @@
+package com.tellingus.tellingme.presentation.ui.feature.home
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+//@HiltViewModel
+//class HomeViewModel @Inject constructor(
+//
+//) {
+//}
