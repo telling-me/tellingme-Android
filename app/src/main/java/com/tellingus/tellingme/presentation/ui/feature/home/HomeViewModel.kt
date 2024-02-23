d289038558e03897cb334d39901bc35883cd8476
장현봉 <wkdgusqhd080@naver.com>
2024-02-24 00:04:18 +0900

    chore: HomeContract state 생성

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/home/HomeViewModel.kt
@@ -3,8 +3,14 @@ package com.tellingus.tellingme.presentation.ui.feature.home
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-//@HiltViewModel
-//class HomeViewModel @Inject constructor(
-//
-//) {
-//}
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+
+) {
+    val state = HomeContract.State(
+        isLoading = false,
+        todayQuestionCardInfo = HomeContract.State.TodayQuestionCardInfo("", "")
+    )
+
+
+}
