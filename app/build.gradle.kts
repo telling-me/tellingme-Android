3287e57d98df47eb081685d61df606f32ac5007b
장현봉 <wkdgusqhd080@naver.com>
2024-02-23 23:34:11 +0900

    chore: HorizontalPager 생성자 parameter 에러로 인한 compose foundation 종속성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -109,6 +109,9 @@ dependencies {
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
+    // Compose base
+    implementation("androidx.compose.foundation:foundation:1.6.2")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
