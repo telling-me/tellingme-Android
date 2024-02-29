805872764f182931037fb3d09a60dc5809bdaddd
Ojongseok <whdtjr6889@naver.com>
2024-02-29 14:28:42 +0900

    feature: SignupViewModel 생성 및 State 정의

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupViewModel.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupViewModel.kt
@@ -1,4 +1,26 @@
 package com.tellingus.tellingme.presentation.ui.feature.auth.signup
 
-class SignupViewModel {
+import com.tellingus.tellingme.presentation.ui.common.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignupViewModel @Inject constructor(
+
+): BaseViewModel<SignupContract.State, SignupContract.Event, SignupContract.Effect>(
+    initialState = SignupContract.State()
+) {
+
+    fun initLoginInfo(socialId: String, socialLoginType: String) {
+        updateState(currentState.copy(
+            socialId = socialId,
+            socialLoginType = socialLoginType
+        ))
+    }
+
+    override fun reduceState(event: SignupContract.Event) {
+
+    }
+
+
 }
