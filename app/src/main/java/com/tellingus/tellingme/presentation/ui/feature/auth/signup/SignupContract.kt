e4da9dabb1711371cd2d060bc4353c1cb9c05e01
Ojongseok <whdtjr6889@naver.com>
2024-02-29 14:28:42 +0900

    feature: SignupViewModel 생성 및 State 정의

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupContract.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupContract.kt
@@ -1,13 +1,19 @@
 package com.tellingus.tellingme.presentation.ui.feature.auth.signup
 
-import com.tellingus.tellingme.data.model.oauth.login.OauthRequestDto
 import com.tellingus.tellingme.presentation.ui.common.base.UiEffect
 import com.tellingus.tellingme.presentation.ui.common.base.UiEvent
 import com.tellingus.tellingme.presentation.ui.common.base.UiState
 
 class SignupContract {
     data class State(
-        val signupContents: OauthRequestDto = OauthRequestDto()
+        val socialId: String = "",
+        val socialLoginType: String = "",
+        val nickname: String = "",
+        val gender: String = "",
+        val birthDate: String = "",
+        val purpose: String = "",
+        val job: Int = 0,
+        val jobInfo: String = "",
     ): UiState
 
     sealed class Event: UiEvent {
