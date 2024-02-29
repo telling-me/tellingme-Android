c008d49e13fde43069c37ab9ca14dfbe67f188ae
Ojongseok <whdtjr6889@naver.com>
2024-03-01 03:10:49 +0900

    build: lifecycle 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,9 @@ dependencies {
     // Compose base
     implementation("androidx.compose.foundation:foundation:1.6.2")
 
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
