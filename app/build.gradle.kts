01f253429f5445239500ac4a4fce382e18df5b46
Ojongseok <whdtjr6889@naver.com>
2024-03-02 02:32:55 +0900

    build: 외부 BottomSheet 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,6 +116,9 @@ dependencies {
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
+    // BottomSheet
+    implementation("com.holix.android:bottomsheetdialog-compose:1.2.3")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
