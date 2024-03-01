cd9358619d728da707e833a360d7fbb99bd7b6d3
Ojongseok <whdtjr6889@naver.com>
2024-03-02 01:18:00 +0900

    feature: 고민은 최대 2가지 선택할 수 있도록 로직 작성

--- app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupWorryScreen.kt
+++ app/src/main/java/com/tellingus/tellingme/presentation/ui/feature/auth/signup/SignupWorryScreen.kt
@@ -2,9 +2,6 @@ package com.tellingus.tellingme.presentation.ui.feature.auth.signup
 
 import android.util.Log
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -13,11 +10,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -35,15 +32,14 @@ import com.tellingus.tellingme.presentation.ui.common.component.button.PrimaryBu
 import com.tellingus.tellingme.presentation.ui.common.component.button.TellingmeIconButton
 import com.tellingus.tellingme.presentation.ui.common.component.layout.MainLayout
 import com.tellingus.tellingme.presentation.ui.common.model.ButtonSize
-import com.tellingus.tellingme.presentation.ui.common.navigation.AuthDestinations
 import com.tellingus.tellingme.presentation.ui.theme.Base0
 import com.tellingus.tellingme.presentation.ui.theme.Gray500
 import com.tellingus.tellingme.presentation.ui.theme.Gray600
+import com.tellingus.tellingme.presentation.ui.theme.Primary400
 import com.tellingus.tellingme.presentation.ui.theme.TellingmeTheme
-import com.tellingus.tellingme.util.collectWithLifecycle
 
 @Composable
-fun SignupJobScreen(
+fun SignupWorryScreen(
     navController: NavController,
     viewModel: SignupViewModel = hiltViewModel(),
     modifier: Modifier = Modifier
@@ -67,7 +63,7 @@ fun SignupJobScreen(
             )
         },
         content = {
-            SignupJobContentScreen(
+            SignupWorryContentScreen(
                 navController = navController,
                 viewModel = viewModel
             )
@@ -78,32 +74,39 @@ fun SignupJobScreen(
 }
 
 @Composable
-fun SignupJobContentScreen(
+fun SignupWorryContentScreen(
     navController: NavController,
     viewModel: SignupViewModel,
     modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    var selectedJob by remember { mutableStateOf(-1) }
+//    var selectedWorry by remember { mutableStateOf(emptyList<Int>()) }
+    var selectedWorry = remember { mutableStateListOf<Int>() }
 
-    Log.d("taag job", uiState.joinRequestDto.toString())
+    Log.d("taag worry", uiState.joinRequestDto.toString())
 
-    val jobList = listOf<Job>(
-        Job(R.drawable.icon_bagpack, "중·고등학생"),
-        Job(R.drawable.icon_graduationcap, "대학(원)생"),
-        Job(R.drawable.icon_smiley, "취업준비생"),
-        Job(R.drawable.icon_briefcase, "직장인"),
-        Job(R.drawable.icon_cookpot, "주부"),
-        Job(R.drawable.icon_etc, "기타"),
+    val worryList = listOf<Worry>(
+        Worry(R.drawable.icon_pen, "학업/진로"),
+        Worry(R.drawable.icon_handshake, "대인관계"),
+        Worry(R.drawable.icon_values, "성격/가치관"),
+        Worry(R.drawable.icon_magnet, "행동/습관"),
+        Worry(R.drawable.icon_health, "건강"),
+        Worry(R.drawable.icon_etc, "기타"),
     )
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(top = 30.dp, bottom = 16.dp, start = 20.dp, end = 20.dp)
+            .padding(top = 10.dp, bottom = 16.dp, start = 20.dp, end = 20.dp)
     ) {
         Text(
-            text = "직업을 선택해주세요",
+            text = "최대 2가지 선택 가능",
+            style = TellingmeTheme.typography.caption1Bold,
+            color = Primary400
+        )
+        Spacer(modifier = Modifier.size(2.dp))
+        Text(
+            text = "고민을 선택해주세요",
             style = TellingmeTheme.typography.head2Bold,
             color = Gray600
         )
@@ -117,15 +120,20 @@ fun SignupJobContentScreen(
 
         LazyColumn() {
             itemsIndexed(
-                items = jobList,
+                items = worryList,
 //                key = jobList.is
             ) { index, item ->
                 SelectBox(
                     text = item.text,
                     icon = item.icon,
-                    isSelected = index == selectedJob,
+                    isSelected = selectedWorry.contains(index),
                     onClick = {
-                        selectedJob = index
+                        if (selectedWorry.size<2) {
+                            selectedWorry.add(index)
+                        } else {
+                            selectedWorry.removeAt(0)
+                            selectedWorry.add(index)
+                        }
                     }
                 )
                 Spacer(modifier = modifier.size(12.dp))
@@ -140,26 +148,16 @@ fun SignupJobContentScreen(
                 .fillMaxWidth(),
             size = ButtonSize.LARGE,
             text = "다음",
-            enable = selectedJob != -1,
+            enable = selectedWorry.size != 0,
             onClick = {
-                viewModel.processEvent(
-                    SignupContract.Event.NextButtonClickedInJob(job = selectedJob)
-                )
+//                viewModel.processEvent()
             }
         )
     }
 
-    viewModel.effect.collectWithLifecycle { effect ->
-        when(effect) {
-            is SignupContract.Effect.MoveToWorry -> {
-                navController.navigate(AuthDestinations.Signup.SIGNUP_WORRY)
-            }
-            else -> {}
-        }
-    }
 }
 
-data class Job(
+data class Worry(
     @DrawableRes val icon: Int,
     val text: String,
     var isSelected: Boolean = false
@@ -167,6 +165,6 @@ data class Job(
 
 @Preview
 @Composable
-fun SignupJobScreenPreview() {
-    SignupJobScreen(navController = rememberNavController())
+fun SignupWorryScreenPreview() {
+    SignupWorryScreen(navController = rememberNavController())
 }
